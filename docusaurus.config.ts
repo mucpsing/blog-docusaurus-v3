6b1626c503e918c74a4243e8d43e85c8e16c774d
Capsion <373704015@qq.com>
2025-02-24 23:35:08 +0800

    ✨feat(bubble): 新增泡泡位置在resize事件中的响应，现在还差泡泡范围w和h变更时的响应

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -97,7 +97,7 @@ const config: Config = {
         //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         // },
         theme: {
-          customCss: ["./src/css/custom.css"],
+          customCss: ["./src/css/custom.css", "./src/components/BubbleText/bubble.css", "./src/components/FallingItemsList/FallingItemsList.css"],
         },
       } satisfies Preset.Options,
     ],
