129ff92105c937dab714184e4358f4dd170ad9c4
cpasion-office-win10 <373704015@qq.com>
2025-03-26 20:47:46 +0800

    ⚡️perf(build): 优化打包文件
    自意义修复中

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,21 @@
  * @Author: Capsion 373704015@qq.com
  * @Date: 2025-02-13 22:20:06
  * @LastEditors: cpasion-office-win10 373704015@qq.com
- * @LastEditTime: 2025-03-17 17:03:23
+ * @LastEditTime: 2025-03-26 15:42:25
  * @FilePath: \cps-blog-docusaurus-v3\src\pages\index.tsx
  * @Description:  入口文件，首页
  */
 import React from "react";
-import Home from "./Home";
 import BrowserOnly from "@docusaurus/BrowserOnly";
-import Test from "./Test";
+import Start from "./Start";
+
+import Home from "./Home";
+// import Test from "./Test";
 
 export default function HomePage() {
   return (
-    <BrowserOnly fallback={<div>Loading...</div>}>
+    <BrowserOnly fallback={<Start></Start>}>
+      {/* <BrowserOnly fallback={<div>Loading 123333333 </div>}> */}
       {() => {
         return <Home />;
 
