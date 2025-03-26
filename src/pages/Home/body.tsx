a90db004dd29d70e29003ee3cd932cb33061dc45
Capsion <373704015@qq.com>
2025-03-26 22:27:43 +0800

    ✨feat(homg/swper): 完善横幅滚动和title

--- src/pages/Home/body.tsx
+++ src/pages/Home/body.tsx
@@ -2,7 +2,7 @@
  * @Author: cpasion-office-win10 373704015@qq.com
  * @Date: 2025-02-20 09:21:04
  * @LastEditors: Capsion 373704015@qq.com
- * @LastEditTime: 2025-03-18 10:45:41
+ * @LastEditTime: 2025-03-20 21:10:39
  * @FilePath: \cps-blog-docusaurus-v3\src\pages\Home\body.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
