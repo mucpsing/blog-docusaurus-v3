84a0d53ed0084fc732e834b3be7f4ff7c078b373
Capsion <373704015@qq.com>
2025-04-15 23:17:43 +0800

    ✨feat(background): 优化背景泡泡组件

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,25 @@
 /*
  * @Author: Capsion 373704015@qq.com
  * @Date: 2025-02-13 22:20:06
- * @LastEditors: cpasion-office-win10 373704015@qq.com
- * @LastEditTime: 2025-03-26 15:42:25
+ * @LastEditors: Capsion 373704015@qq.com
+ * @LastEditTime: 2025-03-27 22:12:45
  * @FilePath: \cps-blog-docusaurus-v3\src\pages\index.tsx
  * @Description:  入口文件，首页
  */
-import React from "react";
+import React, { useEffect } from "react";
 import BrowserOnly from "@docusaurus/BrowserOnly";
+import useGlobalData from "@docusaurus/useGlobalData";
 import Start from "./Start";
 
 import Home from "./Home";
+
 // import Test from "./Test";
 
 export default function HomePage() {
+  const globalData = useGlobalData();
+  useEffect(() => {
+    console.log("globalData: ", globalData);
+  });
   return (
     <BrowserOnly fallback={<Start></Start>}>
       {/* <BrowserOnly fallback={<div>Loading 123333333 </div>}> */}
