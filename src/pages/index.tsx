84877d21a125bfdbfc53c6aa5e9751b1a854feee
cpasion-office-win10 <373704015@qq.com>
2025-04-27 15:31:08 +0800

    ✨feat(fast): ✨feat(mp4): 新增天翼云首页效果视频文件
    自意义修复中

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,20 @@
  * @Author: Capsion 373704015@qq.com
  * @Date: 2025-02-13 22:20:06
  * @LastEditors: cpasion-office-win10 373704015@qq.com
- * @LastEditTime: 2025-03-26 15:42:25
+ * @LastEditTime: 2025-04-03 16:02:31
  * @FilePath: \cps-blog-docusaurus-v3\src\pages\index.tsx
  * @Description:  入口文件，首页
  */
 import React from "react";
 import BrowserOnly from "@docusaurus/BrowserOnly";
-import Start from "./Start";
+// import Start from "./Start";
 
 import Home from "./Home";
 // import Test from "./Test";
 
 export default function HomePage() {
   return (
-    <BrowserOnly fallback={<Start></Start>}>
+    <BrowserOnly>
       {/* <BrowserOnly fallback={<div>Loading 123333333 </div>}> */}
       {() => {
         return <Home />;
